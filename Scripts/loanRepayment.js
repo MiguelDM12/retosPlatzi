354c7c805af278f7e71878f01f686df5cee07751
Miguel Di Massimo <miguel.dimassimo@gmail.com>
2022-08-15 21:31:25 +0200

    Terminada calculadora y redondeos

--- Scripts/loanRepayment.js
+++ Scripts/loanRepayment.js
@@ -10,8 +10,6 @@ inputContainer.addEventListener("input", function() {
 //Periodicity
 const inputContainer2 = document.getElementById("slider2");
 const valuesContainer2 = document.getElementById("valueBlock-value2");
-console.log(inputContainer2.value)
-console.log(valuesContainer2.value)
 
 inputContainer2.addEventListener("input", function() {
     valuesContainer2.innerHTML = inputContainer2.value;
@@ -19,31 +17,31 @@ inputContainer2.addEventListener("input", function() {
 
 const totalAmount = document.getElementById("capitalization_amount");
 
-// Calculate Amortization Schedule
+//Calculate Amortization Schedule
 
 function calculateAmortizationSchedule(){
-    let monthlyFees = inputContainer2.value*12;
-    let loanInterest = inputContainer.value/100
+    const outputScheduleLoan = document.getElementById("loanRepayment_output_table");
+    outputScheduleLoan.innerHTML = `<th>Año</th>`+`<th>Cuota</th>`+`<th>Intereses</th>`+`<th>Capital</th>`+`<th>Capital Vivo</th>`;
+    let loanInterest = inputContainer.value/100;
     let amountPayYear = totalAmount.value*loanInterest/(1-(Math.pow(1+loanInterest, -inputContainer2.value)));
-    let amountPayMonth = (amountPayYear/12).toFixed(2);
     console.log(amountPayYear);
-    console.log(amountPayMonth);
-    let month = 0;
+    let year = 0;
     let fee = 0;
     let capital = 0;
     let interest = loanInterest;
     let remainingCapital=totalAmount.value;
-    for (let i = 1; i<monthlyFees; i++){
-        remainingCapital = remainingCapital - capital;
-        month = i;
-        fee = amountPayMonth;
-        interest = remainingCapital*loanInterest/monthlyFees;
-        capital = fee - interest;
-        console.log(remainingCapital);
+    for (let i = 1; i<=inputContainer2.value; i++){
+        year = i;
+        fee = (amountPayYear).toFixed(2);
+        interest = (remainingCapital*loanInterest).toFixed(2);
+        capital = (fee - interest).toFixed(2);      
+        remainingCapital = (remainingCapital - capital).toFixed(2);
+        if(remainingCapital < 1){
+            remainingCapital = 0;
+        }
+        let insertLine = `<tr><td>${year}</td>`+`<td>${fee} €</td>`+`<td>${interest} €</td>`+`<td>${capital} €</td>`+`<td>${remainingCapital} €</td></tr>`;
+        outputScheduleLoan.insertAdjacentHTML("beforeend", insertLine);
     }
 }
-
-
-
 
 
